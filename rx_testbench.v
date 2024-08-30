8f60f37c3f7b5782819270b60ad3e5bb09acb300
Rohitvgupta15 <143114985+Rohitvgupta15@users.noreply.github.com>
2024-08-30 23:56:57 +0530

    Add files via upload

---
+++ rx_testbench.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.08.2024 22:47:33
+// Design Name: 
+// Module Name: rx_testbench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rx_testbench(
+
+    );
+endmodule
