122a7d2353e2967ad42f83b4318de36da66187f8
Rohitvgupta15 <143114985+Rohitvgupta15@users.noreply.github.com>
2024-09-01 02:01:01 +0530

    Update rx_uart.v

--- RTL/rx_uart.v
+++ RTL/rx_uart.v
@@ -19,36 +19,41 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module rx_uart(
     input clk,
+    input reset,
     input u_tx,
     input en_rx,
-    output u_rx_done,
-    output reg [127:0] data_out 
-    );
-    
-    reg [4:0]counter = 5'b0;
-    wire [7:0]uart_slave_data;
-    
-    always @( uart_slave_data)
-       begin
-         if(counter <= 5'd15) begin
-            counter =counter +1;
-            if(u_rx_done)
-               data_out={data_out[119:0],uart_slave_data};
-            end
-                 
-         end
-          
+    output u_rx_done,  
+    output reg [127:0] data_out
+);
+
+    reg [4:0] counter; 
+    wire [7:0] uart_slave_data;
+    reg receiving;
+
     
     recevier r1 (
         .clk(clk),
-        .u_rx(uart_tx), 
+        .u_rx(u_tx),
         .en_rx(en_rx),
         .data(uart_slave_data),
-        .u_rx_done(u_rx_done)
+        .u_rx_done(u_rx_done)  
     );
-    
-    
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            counter <= 5'd0;
+            data_out <= 128'd0;
+            receiving <= 1'b0;
+        end else if (en_rx) begin
+            if (u_rx_done && !receiving && counter <= 16) begin
+                receiving <= 1'b1;
+                data_out <= {data_out[119:0], uart_slave_data};  
+                counter <= counter + 1;
+            end else if (!u_rx_done && receiving) begin
+                receiving <= 1'b0;
+            end
+        end
+    end
 endmodule
